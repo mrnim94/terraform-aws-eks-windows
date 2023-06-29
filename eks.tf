49a783f166c15a4e870e421856baa9eaf5d6eb59
mr nim <mrnim@nimtechnology.com>
2023-06-29 23:51:52 +0700

    remove env

--- eks.tf
+++ eks.tf
@@ -132,7 +132,6 @@ module "eks" {
           # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
           ENABLE_PREFIX_DELEGATION = "true"
           WARM_PREFIX_TARGET       = "1"
-          WARM_IP_TARGET           = "5"
         }
       })
     }
