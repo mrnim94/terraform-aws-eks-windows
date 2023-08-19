831607b0ae21bd3dea62a95a0ad29f0acd8814f7
Mr Nim <mr.nim94@gmail.com>
2023-08-19 13:48:43 +0000

    change cluster id to cluster name

--- output.tf
+++ output.tf
@@ -8,9 +8,9 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
-output "cluster_id" {
-  description = "The unique identifier for the EKS cluster"
-  value       = module.eks.cluster_id
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = module.eks.cluster_name
 }
 
 output "oidc_provider_arn" {
