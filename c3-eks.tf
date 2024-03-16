358b9422b3739fda9bcb216a17f854f51e11a7f8
Mr Nim <mr.nim94@gmail.com>
2024-03-16 08:19:08 +0000

    remove custome AMI

--- c3-eks.tf
+++ c3-eks.tf
@@ -56,7 +56,8 @@ module "eks" {
       #####################
       enable_bootstrap_user_data = true
 
-      post_bootstrap_user_data = var.disable_windows_defender ? chomp(<<-EOT
+      post_bootstrap_user_data = var.disable_windows_defender ? chomp(
+      <<-EOT
       # Add Windows Defender exclusion 
       Set-MpPreference -DisableRealtimeMonitoring $true
 
