93d3958f95193fbe450d7adad3272bd84e0d7a95
Mr Nim <mr.nim94@gmail.com>
2024-03-22 19:24:19 +0000

    remove windows

--- c4-cni.tf
+++ c4-cni.tf
@@ -22,16 +22,16 @@ provider "kubernetes" {
 # eks is spun up with aws-vpc-cni helm chart regardless if it is specified in cluster_addons
 # this config can't be set from terraform that I can see. The best option is to overwrite
 # # the existing configmap with the settings we need.
-resource "kubernetes_config_map_v1_data" "amazon_vpc_cni" {
-  metadata {
-    name      = "amazon-vpc-cni"
-    namespace = "kube-system"
-  }
-  data = {
-    enable-windows-ipam = true
-  }
-  force = true
-  depends_on = [
-    module.eks
-  ]
-}
+# resource "kubernetes_config_map_v1_data" "amazon_vpc_cni" {
+#   metadata {
+#     name      = "amazon-vpc-cni"
+#     namespace = "kube-system"
+#   }
+#   data = {
+#     enable-windows-ipam = true
+#   }
+#   force = true
+#   depends_on = [
+#     module.eks
+#   ]
+# }
