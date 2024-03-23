87fec49dd501035340583d9fc0b5d20e9a95bc50
Mr Nim <mr.nim94@gmail.com>
2024-03-23 17:28:58 +0000

    update depends_on

--- c4-cni.tf
+++ c4-cni.tf
@@ -1,9 +1,15 @@
 data "aws_eks_cluster" "cluster_windows" {
   name = module.eks.cluster_name
+  depends_on = [
+    module.eks
+  ]
 }
 
 data "aws_eks_cluster_auth" "cluster_windows" {
   name = module.eks.cluster_name
+  depends_on = [
+    module.eks
+  ]
 }
 
 provider "kubernetes" {
